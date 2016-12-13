5ea5ffd7741192ef7f2d6663b9c79d579cc587d1
Andrew Myers <afredmyers@gmail.com>
2016-12-12 22:28:26 -0700

    Adds pry-rails and pry-byebug as dev dependencies

--- spec/routing/preservation/events_routes_spec.rb
+++ spec/routing/preservation/events_routes_spec.rb
@@ -6,11 +6,7 @@ describe 'routes for Preservation::EventsController', type: :routing do
     # TODO: The engine is mounted in the test app under '/preservation'. Is
     # there any way to test routes relative to the mount point?
     it 'routes to search interface for preservation events' do
-<<<<<<< 30507351f99d3a0f47d6cf6f27cf190ecdf18a06
       expect(get: '/events').to route_to(controller: 'preservation/events', action: 'index')
-=======
-      expect(get: '/events').to route_to(controller: 'preservation/events', action: 'index', section: '/preservation')
->>>>>>> Adds route for /preservation/events
     end
   end
 end
