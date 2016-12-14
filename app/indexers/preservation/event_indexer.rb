28974a2d47d6b799f5925445383fc13cc1e9a1f1
Andrew Myers <afredmyers@gmail.com>
2016-12-14 14:08:31 -0700

    Fixes Preservation::Event model test
    Adds conditionals to EventIndexer to avoid errors in cases of missing
    properties on the Preservation::Event model.
    
    NOTE: This relies on EventIndexer, so it's really an integration test. Move it
    out of spec/models/preservation/events_spec.rb?
    
    Also:
     * Loads 'pry' automatically for specs.

--- app/indexers/preservation/event_indexer.rb
+++ app/indexers/preservation/event_indexer.rb
@@ -2,21 +2,29 @@ module Preservation
   class EventIndexer < ActiveFedora::IndexingService
     def generate_solr_document
       super.tap do |solr_doc|
+        # TODO: Some of the conditionals checking for existence of properties
+        # may be removed after require those fields with validations on the
+        # model.
 
+        unless object.premis_event_type.empty?
+          # Index the PREMIS event type.
+          # NOTE: the value we index is only the last URI segment, which is a
+          # 3-letter abbreviation.
+          Solrizer.set_field(solr_doc,
+                             'premis_event_type',
+                             shortened_field_val = URI(object.premis_event_type.first.id).path.split('/').last,
+                             :symbol)
+        end
 
-        # Index the PREMIS event type.
-        # NOTE: the value we index is only the last URI segment, which is a 3-letter abbreviation.
-        Solrizer.set_field(solr_doc,
-                           'premis_event_type',
-                           shortened_field_val = URI(object.premis_event_type.first.id).path.split('/').last,
-                           :symbol)
+        unless object.premis_agent.empty?
+          # Index the PREMIS agent, under the assumption that is a mailto: URI.
+          # NOTE: The value we index is only the amil address, without the "mailto:" part.
+          Solrizer.set_field(solr_doc,
+                             'premis_agent',
+                             object.premis_agent.first.id.sub(/^mailto:/, ''),
+                             :symbol)
+        end
 
-        # Index the PREMIS agent, under the assumption that is a mailto: URI.
-        # NOTE: The value we index is only the amil address, without the "mailto:" part.
-        Solrizer.set_field(solr_doc,
-                           'premis_agent',
-                           object.premis_agent.first.id.sub(/^mailto:/, ''),
-                           :symbol)
 
         # If the PreservationEvent's agent is found in the db,
         # then index that user's full name as well.
@@ -29,17 +37,19 @@ module Preservation
         #                      :stored_searchable)
         # end
 
-        # Index the PREMIS event date time as a date.
-        Solrizer.set_field(solr_doc,
-                           'premis_event_date_time',
-                           object.premis_event_date_time.first,
-                           :stored_searchable)
+        unless object.premis_event_date_time.empty?
+          # Index the PREMIS event date time as a date.
+          Solrizer.set_field(solr_doc,
+                             'premis_event_date_time',
+                             object.premis_event_date_time.first,
+                             :stored_searchable)
 
-        # Index the PREMIS event date time as an integer, for range queries.
-        Solrizer.set_field(solr_doc,
-                           'premis_event_date_time_integer',
-                           object.premis_event_date_time.first.strftime('%Y%m%d').to_i,
-                           Solrizer::Descriptor.new(:long, :stored, :indexed))
+            # Index the PREMIS event date time as an integer, for range queries.
+            Solrizer.set_field(solr_doc,
+                               'premis_event_date_time_integer',
+                               object.premis_event_date_time.first.strftime('%Y%m%d').to_i,
+                               Solrizer::Descriptor.new(:long, :stored, :indexed))
+        end
       end
     end
   end
