53089db436bf92bc4f3e85b1ac42c0db7241a776
Andrew Myers <afredmyers@gmail.com>
2016-12-14 23:47:15 -0700

    Adds more specs for search page

--- spec/features/preservation/search_events_spec.rb
+++ spec/features/preservation/search_events_spec.rb
@@ -3,11 +3,23 @@ require 'rails_helper'
 describe 'Preservation Events search page' do
   before { 12.times { create(:event) } }
 
-  context 'with no user action' do
+  context 'default display' do
     before { visit 'preservation/events' }
 
-    it 'displays up 10 results by default' do
+    it 'displays up to 10 results' do
       expect(page).to have_css('li.document', count: 10)
+    end
+
+    xit 'displays the PREMIS type for the title' do
+    end
+
+    it 'displays the PREMIS agent' do
+      expect(page).to have_css('dt.blacklight-premis_agent_ssim', count: 10)
+    end
+
+    it 'displays the date of the event' do
+      # TODO: avoid using hardcoded dynamic solr suffix here
+      expect(page).to have_css('dt.blacklight-premis_event_date_time_dtsim', count: 10)
     end
   end
 end
