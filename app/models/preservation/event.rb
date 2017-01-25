eb4b50fd49a4beb2221f55f2748921950b7de7fb
amolmkhedkar <akhedkar@iu.edu>
2017-01-25 16:33:19 -0500

    Add premis_event_outcome and premis_event_detail

--- app/models/preservation/event.rb
+++ app/models/preservation/event.rb
@@ -7,6 +7,8 @@ module Preservation
     property :premis_event_type, predicate: ::RDF::Vocab::PREMIS.hasEventType
     property :premis_agent, predicate: ::RDF::Vocab::PREMIS.hasAgent
     property :premis_event_date_time, predicate: ::RDF::Vocab::PREMIS.hasEventDateTime
+    property :premis_event_outcome, predicate: ::RDF::Vocab::PREMIS.hasEventOutcome
+    property :premis_event_detail, predicate: ::RDF::Vocab::PREMIS.hasEventDetail   
 
     def user_from_db
       @user_from_db ||= User.where(email: premis_agent)
